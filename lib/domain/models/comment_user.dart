f19aabb5ea3687a90dba9a8b76f69205adbf6d2e
daothikhuyen <daothikhuyen30@gmail.com>
2025-10-03 11:58:36 +0700

    remove fouder domain

--- lib/domain/models/comment_user.dart
+++
@@ -1,8 +0,0 @@
-import 'package:hotel_booking_app/data/model/comment.dart';
-import 'package:hotel_booking_app/data/model/user.dart';
-
-class CommentWithUser {
-  CommentWithUser({required this.comment, required this.user});
-  final Comment comment;
-  final HBUser user;
-}
--- lib/domain/models/destination.dart
+++
@@ -1,7 +0,0 @@
-class Destination {
-  Destination({required this.id, required this.icon, required this.labelKey});
-  
-  String id;
-  String icon;
-  String labelKey;
-}
